fc908e66cc8c885ae6aa5679eb947b2b9936c02a
Sheldon Lobo <sheldon.lobo@pendo.io>
2023-04-08 13:54:35 -0400

    L4: onSale conditional

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            description: "warm and fuzzy",
+            image: './assets/images/socks_blue.jpg',
+            urlx: "https://sheldon.lobo.codes",
+            inventory: 8,
+            onSale: true
         }
     }
 })
